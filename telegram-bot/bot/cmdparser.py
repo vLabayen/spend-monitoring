485fdb6c4ed953140e585d8fecab445d0ae4ccaf
vLabayen <victor.mendi3216@gmail.com>
2023-06-18 00:33:26 +0200

    [refactor] Some comments

--- telegram-bot/bot/cmdparser.py
+++ telegram-bot/bot/cmdparser.py
@@ -10,9 +10,7 @@ from bot.commands import Command
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE, message: str) -> None:
-    ''' Callback for the /help command.
-    Show global application help
-    '''
+    ''' Callback for the showing global & command-specific help '''
     await context.bot.send_message(
         chat_id = update.effective_chat.id,
         text = message
@@ -26,6 +24,7 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE, messages: Li
             text = message
         )
 
+# Exceptions to interrupt parsing & send info to the user
 class ParserHelp(Exception):
     def __init__(self, **kwargs):
         self.kwargs = kwargs
@@ -38,22 +37,26 @@ class ParserError(Exception):
 class CmdParser(argparse.ArgumentParser):
     ignore_args = {'command', 'handler', 'parser', 'help'}
 
+    # Override error & print_help to generate interruptions
     def error(self, message: str, *args, **kwargs):
         raise ParserError(messages = [message, self.format_help()])
 
     def print_help(self, *args, **kwargs) -> None:
         raise ParserHelp(message = self.format_help())
 
+    # Parse args for specific command or go directly to help & error handlers
     def parse_args(self, cmd: str) -> argparse.Namespace:
         try: return super().parse_args(shlex.split(cmd))
         except ParserHelp as e: return argparse.Namespace(handler=help, **e.kwargs)
         except ParserError as e: return argparse.Namespace(handler=error, **e.kwargs)
 
+    # Fetch all arguments for the command exluding global ones
     @staticmethod
     def get_kwargs(args: argparse.Namespace) -> dict:
         return {name: value for name, value in vars(args).items() if name not in CmdParser.ignore_args}
 
 
+# Create a parser for all the given commands & and a global help command
 def create_parser(commands: List[Command]):
     parser = CmdParser(add_help=False, usage=argparse.SUPPRESS)
     subparsers = parser.add_subparsers(dest='command')
